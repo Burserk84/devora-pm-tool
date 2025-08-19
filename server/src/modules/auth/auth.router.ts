5f11af3f35444fc23dd04f3b72f243aaa2d5981d
Burserk84 <amirburserk@gmail.com>
2025-08-19 21:33:22 +0330

    feat: implement Zod validation for registration

--- server/src/modules/auth/auth.router.ts
+++ server/src/modules/auth/auth.router.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { register, login } from "./auth.handler";
+import { validate } from "../../middlewares/validation.middleware";
+import { registerSchema } from "../../lib/schemas";
 
 const authRouter = Router();
 
-authRouter.post("/register", register);
+authRouter.post("/register", validate(registerSchema), register);
 authRouter.post("/login", login);
 
 export default authRouter;
